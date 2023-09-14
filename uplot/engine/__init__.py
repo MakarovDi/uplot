cd6e781a45868a47ff9444c8e1bf0c59b84f27c0
Dmitry Makarov <makarovdmv@gmail.com>
2023-09-15 00:06:33 +0300

    [engine] managing functions for engines

--- uplot/engine/__init__.py
+++ uplot/engine/__init__.py
@@ -1,1 +1,9 @@
 from uplot.engine.MatplotEngine import MatplotEngine
+from uplot.engine.manage import available, register
+
+
+# init default engines
+
+if MatplotEngine.is_available():
+    register(MatplotEngine(),              name='matplot',      short_name='mpl')
+    register(MatplotEngine(backend='agg'), name='matplot-file', short_name='mpl-file')
--- uplot/engine/manage.py
+++ uplot/engine/manage.py
@@ -1,18 +1,27 @@
 from uplot.interface import IPlotEngine
 
-# TODO: StrEnum
+
+DEFAULT_ENGINES: dict[str, IPlotEngine] = { }
+DEFAULT_ENGINES_SHORT_NAME: dict[str, str] = { }
 
 
-DEFAULT_ENGINES: dict[str, IPlotEngine] = {
-    'matplot': ...,
-    'matplot-silent': ..., # for saving plots only
-    'plotly5': ...,
-}
+def available(short_names: bool = False) -> tuple[str, ...]:
+    if short_names:
+        return tuple(DEFAULT_ENGINES_SHORT_NAME.keys())
+    else:
+        return tuple(DEFAULT_ENGINES.keys())
 
 
-def available() -> list[str]:
-    pass
+def register(e: IPlotEngine, name: str, short_name: str = '') -> bool:
+    assert name not in DEFAULT_ENGINES, 'the engine name must be unique'
+    assert short_name not in DEFAULT_ENGINES_SHORT_NAME, 'the engine short name must be unique'
 
+    if not e.is_available():
+        return False
 
-def register(e: IPlotEngine, name: str):
-    pass
+    DEFAULT_ENGINES[name] = e
+
+    if short_name != '':
+        DEFAULT_ENGINES_SHORT_NAME[short_name] = name
+
+    return True
