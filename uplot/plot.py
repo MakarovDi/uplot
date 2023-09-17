5829b55b06dd89c8ddcf120a3ae0a9530d1059cc
Dmitry Makarov <makarovdmv@gmail.com>
2023-09-17 22:41:48 +0300

    [API] fixed figure(None) behavior + documentation

--- uplot/plot.py
+++ uplot/plot.py
@@ -6,8 +6,26 @@ CURRENT_ENGINE: IPlotEngine | None = None
 
 
 def figure(engine: str | IPlotEngine | None = None) -> IFigure:
+    """
+    Creates figure via specified or default engine.
+
+    Parameters
+    ----------
+    engine :
+        plotting engine object or name, if None then return previous or default engine
+
+    Returns
+    -------
+    IFigure
+        figure object
+    """
+    global CURRENT_ENGINE
+
+    if engine is None:
+        engine = CURRENT_ENGINE
+
     if isinstance(engine, IPlotEngine):
         return engine.figure()
 
-    engine = e.get(engine)
-    return engine.figure()
+    CURRENT_ENGINE = e.get(engine)
+    return CURRENT_ENGINE.figure()
