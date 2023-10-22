0ef27468f4497ab6b8d40be3403b900c6dfaeb95
Dmitry Makarov <makarovdmv@gmail.com>
2023-10-22 16:48:28 +0300

    [interface] export functions, classes, modules refactoring

--- uplot/__init__.py
+++ uplot/__init__.py
@@ -1,9 +1,17 @@
 from uplot.info import __version__, __author__, __email__
 
+# engine managing
 import uplot.engine as engine
 
-from uplot.LineStyle import LineStyle
-from uplot.MarkerStyle import MarkerStyle
+# interface
 from uplot.interface import IFigure, IPlotEngine
 
+# main API function
 from uplot.plot import figure
+
+# common routines
+import uplot.color as color
+
+# common types
+from uplot.LineStyle import LineStyle
+from uplot.MarkerStyle import MarkerStyle
