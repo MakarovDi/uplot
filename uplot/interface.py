b68feb254f03617e462b33ae76e023df9bcf1d82
Dmitry Makarov <makarovdmv@gmail.com>
2023-11-04 15:37:09 +0200

    [interface] added @abstract to the interface

--- uplot/interface.py
+++ uplot/interface.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from numpy import ndarray
 from typing import Protocol, runtime_checkable
+from abc import abstractmethod as abstract
 from numpy.typing import ArrayLike
 
 from uplot.LineStyle import LineStyle
@@ -21,17 +22,21 @@ class IFigure(Protocol):
     but very common and familiar for MATLAB users.
     """
     @property
+    @abstract
     def engine(self) -> IPlotEngine:
         return ...
 
     @property
+    @abstract
     def internal(self):
         return ...
 
     @property
+    @abstract
     def is_3d(self) -> bool:
         return ...
 
+    @abstract
     def plot(self, x           : ArrayLike,
                    y           : ArrayLike | None = None,
                    z           : ArrayLike | None = None,
@@ -44,6 +49,7 @@ class IFigure(Protocol):
                    **kwargs):
         ...
 
+    @abstract
     def scatter(self, x           : ArrayLike,
                       y           : ArrayLike | None = None,
                       z           : ArrayLike | None = None,
@@ -55,60 +61,78 @@ class IFigure(Protocol):
                       **kwargs):
         ...
 
+    @abstract
     def imshow(self, image: ArrayLike, **kwargs):
         ...
 
+    @abstract
     def title(self, text: str):
         ...
 
+    @abstract
     def legend(self, show: bool = True, **kwargs):
         ...
 
+    @abstract
     def grid(self, show: bool = True):
         ...
 
+    @abstract
     def xlabel(self, text: str):
         ...
 
+    @abstract
     def ylabel(self, text: str):
         ...
 
+    @abstract
     def zlabel(self, text: str):
         ...
 
+    @abstract
     def xlim(self, min_value: float | None = None,
                    max_value: float | None = None):
         ...
 
+    @abstract
     def ylim(self, min_value: float | None = None,
                    max_value: float | None = None):
         ...
 
+    @abstract
     def zlim(self, min_value: float | None = None,
                    max_value: float | None = None):
         ...
 
+    @abstract
     def current_color(self) -> str:
         ...
 
+    @abstract
     def scroll_color(self, count: int = 1):
         ...
 
+    @abstract
     def reset_color(self):
         ...
 
+    @abstract
     def axis_aspect(self, mode: AspectMode):
         ...
 
+    @abstract
     def as_image(self) -> ndarray:
         ...
 
+    @abstract
     def save(self, fname: str):
         ...
 
+    @abstract
     def close(self):
         ...
 
+    @abstract
     def show(self, block: bool = False):
         ...
 
@@ -116,13 +140,16 @@ class IFigure(Protocol):
 class IPlotEngine(Protocol):
 
     @classmethod
+    @abstract
     def is_available(cls) -> bool:
         ...
 
     @property
+    @abstract
     def figure_type(self) -> type:
         return ...
 
+    @abstract
     def figure(self, width: int, aspect_ratio: float) -> IFigure:
         """
         Factory method for a figure creation
